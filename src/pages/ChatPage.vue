0a7c037ee258e594d563f8ba21656802142c5617
Tegridy Code <marcgabel@live.dk>
2022-03-23 22:26:04 +0100

    Work on Chat View

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,5 +1,15 @@
 <template>
   <q-page class="flex flex-center">
+    <q-footer elevated cen>
+      <q-toolbar>
+        <q-input class="absolute-center" bg-color="white" outlined rounded v-model="text" label="Type here" dense>
+
+          <template v-slot:after>
+            <q-btn color="white" round dense flat icon="send" />
+          </template>
+        </q-input>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
