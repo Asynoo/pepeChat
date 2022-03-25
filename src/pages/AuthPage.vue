d2c440249c53d7b5155e51b7cd91d21061bdca98
Tegridy Code <marcgabel@live.dk>
2022-03-26 00:16:08 +0100

    auth design setup

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,12 +1,46 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="q-pa-md">
+    <q-card>
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <login-register :tab="tab"/>
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <login-register :tab="tab"/>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { ref } from 'vue'
+import LoginRegister from "components/LoginRegister";
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  setup () {
+    return {
+      tab: ref('login')
+    }
+  },
+  components: {
+    'login-register' : require('components/LoginRegister.vue').default
+  }
+}
 </script>
